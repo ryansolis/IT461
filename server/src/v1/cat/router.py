253cf035183d61e5074e765ca63b31ae16c7b245
Ryan Solis <nyarsapril@gmail.com>
2022-05-20 07:00:36 +0800

    applied JWT
    with JWT implementation

--- server/src/v1/cat/router.py
+++ server/src/v1/cat/router.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from v1.cat.controller import CatController
+from v1.auth import jwt_token_required
 
 class CatRouter():
     @staticmethod
-    def handler(app: Blueprint):
+    def handler():
+        app = Blueprint('cats', __name__, url_prefix='/v1/cats')
+        app.before_request(jwt_token_required)
         controller = CatController()
         app.add_url_rule('/', methods=['POST'], view_func=controller.post)
         app.add_url_rule('/', methods=['GET'], view_func=controller.get)
